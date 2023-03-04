eef4c8396876be1cf4231d4fb03031722c1e6fad
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-03-03 21:04:38 -0300

    app refatorado de JSX para TSX

---
+++ src/componentes/Colaborador/index.tsx
@@ -0,0 +1,25 @@
+import { IColaborador } from '../../compartilhado/interfaces/IColaborador';
+import './Colaborador.css'
+
+
+interface ColaboradorProps {
+    nome?: string; 
+    imagem?: string;
+    cargo?: string;
+    corDeFundo: string;
+    colaborador: IColaborador;
+}
+
+export const Colaborador = ({ corDeFundo, colaborador }: ColaboradorProps,) => {
+    return (<div className='colaborador'>
+        <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
+            <img src={colaborador.imagem} alt={colaborador.nome}/>
+        </div>
+        <div className='rodape'>
+            <h4>{colaborador.nome}</h4>
+            <h5>{colaborador.cargo}</h5>
+        </div>
+    </div>)
+}
+
+export default Colaborador
---
+++ src/componentes/Time/index.tsx
@@ -0,0 +1,41 @@
+import { IColaborador } from "../../compartilhado/interfaces/IColaborador";
+import Colaborador from '../Colaborador';
+import "./Time.css";
+
+interface TimeProps {
+    corPrimaria: string;
+    corSecundaria: string;
+    nome: string;
+    colaboradores: IColaborador[];
+    // colaboradores: { nome: string, cargo: string, imagem: string }[]
+    // IColaborador eh uma inteface criada para nao precisar escrever como 
+    //na linha de cima, neste case vale apena criar essa inteface pois 
+    //IColaborador pode ser utilizado em outra parte da aplicacao
+}
+
+
+const Time = (props: TimeProps) => {
+    const css = { backgroundColor: props.corSecundaria };
+
+    return props.colaboradores.length > 0 ? (
+        <section className="time" style={css}>
+            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <div className="colaboradores">
+                {props.colaboradores.map((colaborador) => (
+                    <Colaborador
+                        corDeFundo={props.corPrimaria} 
+                        key={colaborador.nome} 
+                        // nome={colaborador.nome} 
+                        // cargo={colaborador.cargo} 
+                        // imagem={colaborador.imagem} 
+                        colaborador={colaborador}
+                    />
+                ))}
+            </div>
+        </section>
+    ) : (
+        <></>
+    );
+};
+
+export default Time;
